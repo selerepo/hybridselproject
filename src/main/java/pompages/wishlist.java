91f26bf24b09e893bca0b08d15fdcf41018d0fac
hamsa <hamsa@DESKTOP-CSDR9DJ.home>
2022-12-13 21:25:14 -0500

    wishlist

--- src/main/java/pompages/wishlist.java
+++ src/main/java/pompages/wishlist.java
@@ -1,5 +1,13 @@
 package pompages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class wishlist {
+	
+	@FindBy(xpath="//span[text()='Play']")
+	private WebElement play;
+	
+//	@FindBy
 
 }
